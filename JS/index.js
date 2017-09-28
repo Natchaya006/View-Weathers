7b8f11c898fd4e84ef6646b8f94eb5d33e6620db
Natchaya <pinzarzaa39@hotmail.com>
2017-09-29 01:30:15 +0700

    add img and modifile addditional all file

--- JS/index.js
+++ JS/index.js
@@ -16,7 +16,14 @@ $(function () {
             if (picCheck == "Clear") {
                 var picShow = "Weather-Sun-icon.png";
             }
-            var detail = "<table><tr><td width = '350px'><img src='" + picShow + "' alt=''></td><td width = '350px'><img src='Celsius-icon.png' alt=''></td></tr><tr><td width = '300px'><h4>" + data.weather[0].main + "</h4></td><td width = '300px'><h4>" + (data.main.temp) / 10 + "</h4></td></tr></table>";
+            if (picCheck == "Thunderstorm") {
+                var picShow = "37891-200.png";
+            }
+            if (picCheck == "Mist") {
+                var picShow = "Weather-Fog-Day-icon.png";
+            }
+            console.log(data);
+            var detail = "<table align = 'center'><tr><td><img class = 'font' src='" + picShow + "' alt=''></td></tr></table><table align = 'center'><tr><td width = '100%'><p id = 'text'><b>" + data.weather[0].description + "<b></p></td></tr></table>" + "<table>" + "<tr><td><img src='Celsius-icon.png' alt=''></td><td>" + ((data.main.temp) / 10).toFixed(2) + " °C</td></tr>" + "<tr><td><img src='1841-200.png' alt=''></td><td>" + data.main.pressure + " hPa</td></tr>" + "<tr><td><img src='219816.png' alt=''></td><td>" + data.main.humidity + " %</td></tr></table>";
             $("#show").append(detail);
         });
     });
